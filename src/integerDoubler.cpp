5e7b049e2624a99253273ff55700a1fdb0fc0e79
Owen Colley <owen.h.colley@gmail.com>
2025-10-02 13:12:36 -0400

    Use int

--- src/integerDoubler.cpp
+++ src/integerDoubler.cpp
@@ -1,5 +1,5 @@
 #include "integerDoubler.hpp"
-#include <cstdint>
+// #include <cstdint>
 #include <fstream>
 #include <iostream>
 #include <vector>
@@ -14,11 +14,11 @@ void integerDoubler(std::string input_file, std::string output_file) {
   std::ofstream out(output_file);
 
   while (!in.eof()) {
-    int32_t n = 0;
-    in.read(reinterpret_cast<char *>(&n), 4);
+    int n = 0;
+    in.read(reinterpret_cast<char *>(&n), sizeof(int));
     // int64_t n2 = static_cast<int64_t>(n) * 2;
     // std::cout << "2x: " << n << " " << n2 << "\n";
     n *= 2;
-    out.write(reinterpret_cast<char *>(&n), 4);
+    out.write(reinterpret_cast<char *>(&n), sizeof(int));
   }
 }
