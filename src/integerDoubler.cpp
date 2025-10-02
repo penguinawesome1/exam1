978c222003cf749fb9a3f821ba85f62bea873819
Owen Colley <owen.h.colley@gmail.com>
2025-10-02 13:24:54 -0400

    Fix everything lol

--- src/integerDoubler.cpp
+++ src/integerDoubler.cpp
@@ -4,13 +4,13 @@
 #include <vector>
 
 void integerDoubler(std::string input_file, std::string output_file) {
-  std::ifstream in(input_file);
+  std::ifstream in(input_file, std::ios::binary);
   if (!in) {
     std::cerr << "Invalid input path found for integerDoubler\n";
     return;
   }
 
-  std::ofstream out(output_file);
+  std::ofstream out(output_file, std::ios::binary);
 
   in.seekg(1);
 
@@ -18,6 +18,6 @@ void integerDoubler(std::string input_file, std::string output_file) {
     int n = 0;
     in.read(reinterpret_cast<char *>(&n), sizeof(int));
     n *= 2;
-    out.write(reinterpret_cast<char *>(&n), std::ios::binary);
+    out.write(reinterpret_cast<char *>(&n), sizeof(int));
   }
 }
